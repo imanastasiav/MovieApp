93302476a31b660d895f46122ce13fa41f831e54
imanastasiav <imanastasiav@mail.ru>
2022-11-25 20:11:45 +0600

    feat: add pop-up

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const API_KEY = 'a4dd694c-6cc8-41e4-8288-0b6e33706efd';
 const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
 const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
+const API_MOVIE_DETAILS = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/';
 
 getMovies(API_URL_POPULAR);
 
@@ -51,6 +52,7 @@ function showMovies(data) {
         `
       }
     </div>`;
+    movieEl.addEventListener("click", () => openModal(movie.filmId))
     moviesEl.appendChild(movieEl);
   });
 }
@@ -67,5 +69,62 @@ form.addEventListener("submit", (e) => {
     getMovies(apiSearchUrl);
 
     search.value = "";
+  }
+})
+
+//Модальное окно
+const modalEl = document.querySelector('.modal');
+
+async function openModal(id) {
+  const resp = await fetch(API_MOVIE_DETAILS + id, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY
+    }
+  });
+  const respData = await resp.json();
+
+  modalEl.classList.add('modal--show');
+  document.body.classList.add('stop-scrolling');
+
+  modalEl.innerHTML = `
+    <div class="modal__card">
+      <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+      <h2>
+        <span class="modal__movie-title">${respData.nameRu}</span>
+        <span class="modal__movie-release-year">${respData.year} г.</span>
+      </h2>
+      <ul class="modal__movie-info">
+        <div class="loader"></div>
+        <li class="modal__movie-genre">Жанр - ${respData.genres.map((genre) => ` ${genre.genre}`)}</li>
+        ${respData.filmLength ?
+        `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ""}
+        <li>Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
+        <li class="modal__movie-overview">Описание - ${respData.description}</li>
+      </ul>
+      <button type="button" class="modal__button-close">Закрыть</button>
+    </div>
+  `
+  const btnClose = document.querySelector('.modal__button-close');
+  btnClose.addEventListener('click', () => closeModal());
+}
+
+function closeModal() {
+  modalEl.classList.remove('modal--show');
+  document.body.classList.remove('stop-scrolling');
+}
+
+//при клике вне окна
+window.addEventListener('click', (e) => {
+  if (e.target === modalEl) {
+    closeModal();
+  }
+})
+
+//обработчик на кнопку, при нажатии на кнопку esc, окно закрывается
+window.addEventListener('keydown', (e) => {
+  console.log(e.keyCode);
+  if (e.keyCode === 27) {
+    closeModal();
   }
 })
